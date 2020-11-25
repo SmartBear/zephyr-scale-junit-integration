59f06afe451a799302a4b401184fe61a906eac4e
Otavio Medeiros <otaviorm@gmail.com>
2020-11-25 11:23:55 -0300

    JQA-9176 Tweak maven and circleci connfig

---
+++ gen-gpg-key.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+gpg --batch --gen-key <<EOF
+Key-Type: 1
+Key-Length: 2048
+Subkey-Type: 1
+Subkey-Length: 2048
+Name-Real: zephyrscale
+Name-Email: tm4j.bot@gmail.com
+Expire-Date: 0
+Passphrase: ${GPG_PASSPHRASE}
+EOF
