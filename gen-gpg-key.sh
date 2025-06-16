ca155d34d820ae195f30271d5c87c2a7a30bc466
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2025-06-16 14:54:58 +0200

    [OP-32198] Introduce GHA (#114)

--- gen-gpg-key.sh
+++ gen-gpg-key.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ -z "$GPG_PASSPHRASE" ]]; then
+  echo "GPG_PASSPHRASE is not set. Please set it before running this script."
+  exit 1
+fi
+
 gpg --batch --gen-key <<EOF
 Key-Type: 1
 Key-Length: 2048
@@ -8,7 +13,12 @@ Subkey-Length: 2048
 Name-Real: zephyrscale
 Name-Email: svc_zephyr_scale@smartbear.com
 Expire-Date: 0
-Passphrase: ${GPG_PASSPHRASE}
+Passphrase: $GPG_PASSPHRASE
 EOF
 
-gpg --keyserver hkps://keys.openpgp.org --send-keys $(gpg --with-colons --fingerprint | awk -F: '$1 == "fpr" {print $10;}')
+FINGERPRINT=$(gpg --with-colons --fingerprint | awk -F: '$1 == "fpr" {print $10;}')
+if [[ -z "$FINGERPRINT" ]]; then
+  echo "No GPG key fingerprint found. Please check if the key was generated successfully."
+  exit 1
+fi
+gpg --keyserver hkps://keys.openpgp.org --send-keys $FINGERPRINT
